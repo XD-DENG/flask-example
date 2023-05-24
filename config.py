a7cc2a6ad9d73d006a770d91b3400199bddf4136
bjornarron <55078095+bjornarron@users.noreply.github.com>
2023-05-24 14:20:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = "fdsafasd"
 UPLOAD_FOLDER = "image_pool"
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
-SQLALCHEMY_DATABASE_URI = 'postgresql://db:db@localhost:5432/postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/postgres'
