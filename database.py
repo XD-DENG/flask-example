89dfdf2ec31afa4fa85a84fe8c364edd6825d6f7
bjornarron <55078095+bjornarron@users.noreply.github.com>
2023-05-24 14:56:17 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,163 +1,120 @@
-import sqlite3
+import psycopg2
 import hashlib
 import datetime
 
-user_db_file_location = "database_file/users.db"
-note_db_file_location = "database_file/notes.db"
-image_db_file_location = "database_file/images.db"
+# PostgreSQL connection details
+conn_params = {
+    "host": "localhost",
+    "port": "5432",
+    "database": "postgres",
+    "user": "postgres",
+    "password": "postgres",
+}
 
 def list_users():
-    _conn = sqlite3.connect(user_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    _c.execute("SELECT id FROM users;")
-    result = [x[0] for x in _c.fetchall()]
+    cur.execute("SELECT id FROM users;")
+    result = [x[0] for x in cur.fetchall()]
 
-    _conn.close()
-    
+    conn.close()
+
     return result
 
 def verify(id, pw):
-    _conn = sqlite3.connect(user_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    _c.execute("SELECT pw FROM users WHERE id = '" + id + "';")
-    result = _c.fetchone()[0] == hashlib.sha256(pw.encode()).hexdigest()
-    
-    _conn.close()
+    cur.execute("SELECT pw FROM users WHERE id = %s;", (id,))
+    result = cur.fetchone()[0] == hashlib.sha256(pw.encode()).hexdigest()
+
+    conn.close()
 
     return result
 
 def delete_user_from_db(id):
-    _conn = sqlite3.connect(user_db_file_location)
-    _c = _conn.cursor()
-    _c.execute("DELETE FROM users WHERE id = '" + id + "';")
-    _conn.commit()
-    _conn.close()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    # when we delete a user FROM database USERS, we also need to delete all his or her notes data FROM database NOTES
-    _conn = sqlite3.connect(note_db_file_location)
-    _c = _conn.cursor()
-    _c.execute("DELETE FROM notes WHERE user = '" + id + "';")
-    _conn.commit()
-    _conn.close()
+    cur.execute("DELETE FROM users WHERE id = %s;", (id,))
+    conn.commit()
 
-    # when we delete a user FROM database USERS, we also need to 
-    # [1] delete all his or her images FROM image pool (done in app.py)
-    # [2] delete all his or her images records FROM database IMAGES
-    _conn = sqlite3.connect(image_db_file_location)
-    _c = _conn.cursor()
-    _c.execute("DELETE FROM images WHERE owner = '" + id + "';")
-    _conn.commit()
-    _conn.close()
+    cur.execute("DELETE FROM notes WHERE user = %s;", (id,))
+    conn.commit()
+
+    cur.execute("DELETE FROM images WHERE owner = %s;", (id,))
+    conn.commit()
+
+    conn.close()
 
 def add_user(id, pw):
-    _conn = sqlite3.connect(user_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    _c.execute("INSERT INTO users values(?, ?)", (id.upper(), hashlib.sha256(pw.encode()).hexdigest()))
-    
-    _conn.commit()
-    _conn.close()
+    cur.execute("INSERT INTO users (id, pw) VALUES (%s, %s);", (id.upper(), hashlib.sha256(pw.encode()).hexdigest()))
+    conn.commit()
+
+    conn.close()
 
 def read_note_from_db(id):
-    _conn = sqlite3.connect(note_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    command = "SELECT note_id, timestamp, note FROM notes WHERE user = '" + id.upper() + "';" 
-    _c.execute(command)
-    result = _c.fetchall()
+    command = "SELECT note_id, timestamp, note FROM notes WHERE user = %s;"
+    cur.execute(command, (id.upper(),))
+    result = cur.fetchall()
 
-    _conn.commit()
-    _conn.close()
+    conn.close()
 
     return result
 
 def match_user_id_with_note_id(note_id):
-    # Given the note id, confirm if the current user is the owner of the note which is being operated.
-    _conn = sqlite3.connect(note_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    command = "SELECT user FROM notes WHERE note_id = '" + note_id + "';" 
-    _c.execute(command)
-    result = _c.fetchone()[0]
+    command = "SELECT user FROM notes WHERE note_id = %s;"
+    cur.execute(command, (note_id,))
+    result = cur.fetchone()[0]
 
-    _conn.commit()
-    _conn.close()
+    conn.close()
 
     return result
 
 def write_note_into_db(id, note_to_write):
-    _conn = sqlite3.connect(note_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
     current_timestamp = str(datetime.datetime.now())
-    _c.execute("INSERT INTO notes values(?, ?, ?, ?)", (id.upper(), current_timestamp, note_to_write, hashlib.sha1((id.upper() + current_timestamp).encode()).hexdigest()))
+    cur.execute(
+        "INSERT INTO notes (user, timestamp, note, note_id) VALUES (%s, %s, %s, %s);",
+        (id.upper(), current_timestamp, note_to_write, hashlib.sha1((id.upper() + current_timestamp).encode()).hexdigest()),
+    )
+    conn.commit()
 
-    _conn.commit()
-    _conn.close()
+    conn.close()
 
 def delete_note_from_db(note_id):
-    _conn = sqlite3.connect(note_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    command = "DELETE FROM notes WHERE note_id = '" + note_id + "';" 
-    _c.execute(command)
+    command = "DELETE FROM notes WHERE note_id = %s;"
+    cur.execute(command, (note_id,))
+    conn.commit()
 
-    _conn.commit()
-    _conn.close()
+    conn.close()
 
 def image_upload_record(uid, owner, image_name, timestamp):
-    _conn = sqlite3.connect(image_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    _c.execute("INSERT INTO images VALUES (?, ?, ?, ?)", (uid, owner, image_name, timestamp))
+    cur.execute("INSERT INTO images (uid, owner, name, timestamp) VALUES (%s, %s, %s, %s);", (uid, owner, image_name, timestamp))
+    conn.commit()
 
-    _conn.commit()
-    _conn.close()
+    conn.close()
 
 def list_images_for_user(owner):
-    _conn = sqlite3.connect(image_db_file_location)
-    _c = _conn.cursor()
+    conn = psycopg2.connect(**conn_params)
+    cur = conn.cursor()
 
-    command = "SELECT uid, timestamp, name FROM images WHERE owner = '{0}'".format(owner)
-    _c.execute(command)
-    result = _c.fetchall()
-
-    _conn.commit()
-    _conn.close()
-
-    return result
-
-def match_user_id_with_image_uid(image_uid):
-    # Given the note id, confirm if the current user is the owner of the note which is being operated.
-    _conn = sqlite3.connect(image_db_file_location)
-    _c = _conn.cursor()
-
-    command = "SELECT owner FROM images WHERE uid = '" + image_uid + "';" 
-    _c.execute(command)
-    result = _c.fetchone()[0]
-
-    _conn.commit()
-    _conn.close()
-
-    return result
-
-def delete_image_from_db(image_uid):
-    _conn = sqlite3.connect(image_db_file_location)
-    _c = _conn.cursor()
-
-    command = "DELETE FROM images WHERE uid = '" + image_uid + "';" 
-    _c.execute(command)
-
-    _conn.commit()
-    _conn.close()
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    print(list_users())
+    command = "SELECT uid, timestamp, name
